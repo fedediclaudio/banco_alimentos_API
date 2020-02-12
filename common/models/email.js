b645b0968c625bdb30e59fb46cf0f1b54d6fedd8
Valentin <vccarp@gmail.com>
2020-02-11 22:53:14 -0300

    email

--- common/models/email.js
+++ common/models/email.js
@@ -1,16 +1,5 @@
-"use strict";
-module.exports = function(MyModel) {
-    // send an email
-    MyModel.sendEmail = function(cb) {
-      MyModel.app.models.Email.send({
-        to: 'foo@bar.com',
-        from: 'you@gmail.com',
-        subject: 'my subject',
-        text: 'my text',
-        html: 'my <em>html</em>'
-      }, function(err, mail) {
-        console.log('email sent!');
-        cb(err);
-      });
-    }
-  };
+'use strict';
+
+module.exports = function(Email) {
+
+};
